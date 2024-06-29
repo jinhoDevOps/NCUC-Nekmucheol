5bb93032e127602dbe6878e8feb5302b3bb00e23
Tekk-97 <jo22033@gmail.com>
2024-06-29 15:48:48 +0900

    first

---
+++ FirstPersona.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+
+class CompletionExecutor:
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
+
+    def execute(self, completion_request,belif):
+        headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'text/event-stream'
+        }
+        capture_next = False
+
+        with requests.post(self._host + '/testapp/v1/chat-completions/HCX-003',
+                           headers=headers, json=completion_request, stream=True) as r:
+            for line in r.iter_lines():
+                if line:
+                    decoded_line = line.decode("utf-8")
+                    # print(decoded_line)
+                    if capture_next:
+                        if decoded_line.startswith('data:'):
+                            json_data = decoded_line[len('data:'):]
+                            result_data = json.loads(json_data)
+                            content_data = result_data.get('message', {}).get('content')
+                            break
+                    if decoded_line.startswith('event:result'):
+                        capture_next = True
+
+        return content_data
+def create_preset_text(input_list,belif):
+    roles = ["user", "assistant"]
+    preset_text = [{"role":"system","content":"- [배경] 상황내에서 이야기 해야합니다.\n- [1번] 문장에만 공감해야합니다.\n\n- [2번] 문장에는 반박해야합니다.\n"
+                                              "- 대화형으로 말해야합니다\n\n\n\n\n--------------------\n예시1)\n질문)\n[배경] 우리 집에 밤마다 귀신이 나타나는데 그 귀신이 박보영을 닮았다면 이사 가야 되나 말아야 되나? "
+                                              "\r\n\r\n[1번] 이사를 가는게 좋아\r\n1. 귀신 때문에 일상생활에 지장이 생길 수 있어요. 안전을 위해서라도 이사를 가는 것이 좋을 것 같아요.\r\n\r\n[2번] 이사를 가지 않는게 좋아"
+                                              "\r\n1. 귀신이 나타날 때마다 대처할 방법을 찾아보거나, 귀신에게 도움을 요청해볼 수도 있지 않을까요?"
+                                              "\n\n\n답변)\n무조건 귀신을 피해 사는게 좋아. 왜냐하면 귀신한테 죽으면 말짱 도루묵이야. 귀신을 피하고 대처할 방법을 찾는건 현실적으로 말이 안돼"}]
+
+    for i, content in enumerate(input_list):
+        role = roles[i % 2]  # 역할이 번갈아가며 지정됨
+        preset_text.append({"role": role, "content": content})
+    # print(preset_text)
+    preset_text.append({"role":"user", "content": belif+content[-1]})
+
+
+    return preset_text
+
+
+def FirstPersona(prompt,belif):
+    completion_executor = CompletionExecutor(
+        host='https://clovastudio.stream.ntruss.com',
+        api_key='NTA0MjU2MWZlZTcxNDJiYyIdXae7qo9JXcvUXKd70BvjmRLLOGbGh2Jo56OFZ9Et',
+        api_key_primary_val='a7J5bHPGxJUvPVBCT8l4urszZOP6PpLPONm0vjyF',
+        request_id='6d4d8787-4a53-4a55-a898-c5b725aa4f9b'
+    )
+
+    preset_text = create_preset_text(prompt,belif)
+
+    request_data = {
+        'messages': preset_text,
+        'topP': 0.8,
+        'topK': 0,
+        'maxTokens': 256,
+        'temperature': 0.64,
+        'repeatPenalty': 5.0,
+        'stopBefore': [],
+        'includeAiFilters': False,
+        'seed': 0
+    }
+
+    # print(preset_text)
+    # completion_executor.execute(request_data)
+    result = completion_executor.execute(request_data,belif)
+    return result
---
+++ makePersona.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+
+class CompletionExecutor:
+    def __init__(self, host, api_key, api_key_primary_val, request_id):
+        self._host = host
+        self._api_key = api_key
+        self._api_key_primary_val = api_key_primary_val
+        self._request_id = request_id
+
+    def execute(self, completion_request):
+        headers = {
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Accept': 'text/event-stream'
+        }
+        capture_next = False
+
+        with requests.post(self._host + '/testapp/v1/chat-completions/HCX-DASH-001',
+                           headers=headers, json=completion_request, stream=True) as r:
+            for line in r.iter_lines():
+                if line:
+                    decoded_line = line.decode("utf-8")
+                    # print(decoded_line)
+                    if capture_next:
+                        if decoded_line.startswith('data:'):
+                            json_data = decoded_line[len('data:'):]
+                            result_data = json.loads(json_data)
+                            content_data = result_data.get('message', {}).get('content')
+                            break
+                    if decoded_line.startswith('event:result'):
+                        capture_next = True
+
+        print(type(content_data))
+        # content_data를 튜플로 분리
+        if content_data:
+            parts = content_data.split('\n\n')
+            if len(parts) >= 2:
+                return list(parts)
+
+        return None
+
+
+        # with requests.post(self._host + '/testapp/v1/chat-completions/HCX-DASH-001',
+        #                    headers=headers, json=completion_request, stream=True) as r:
+        #     for line in r.iter_lines():
+        #         if line:
+        #             print(line.decode("utf-8"))
+
+
+def extract_event_result_part(text):
+    lines = text.strip().split("\n")
+    event_result_part = []
+    capture = False
+
+    for line in lines:
+        if line.startswith("event:result"):
+            capture = True
+        if capture:
+            event_result_part.append(line)
+
+    return "\n".join(event_result_part)
+def makePersona(prompt):
+    completion_executor = CompletionExecutor(
+        host='https://clovastudio.stream.ntruss.com',
+        api_key='NTA0MjU2MWZlZTcxNDJiYyIdXae7qo9JXcvUXKd70BvjmRLLOGbGh2Jo56OFZ9Et',
+        api_key_primary_val='a7J5bHPGxJUvPVBCT8l4urszZOP6PpLPONm0vjyF',
+        request_id='039826c9-347d-45d4-a5f5-26cdc3cd1e0b'
+    )
+
+    preset_text = [{"role":"system","content":"- 의견에서 어떤 관점이 있는지 분류하는 AI 입니다.\n- 의견은 2가지로 나와야 하며, 다른 AI 명령 프롬폼트의 그 내용을 넣을꺼야\n- 항상 명령체로 글을 만들어줘\n\n\n--------------------------------------------------\n\n\n\n\n예시 1) \n우리 집에 밤마다 귀신이 나타나는데 그 귀신이 박보영을 닮았다면 이사 가야 되나 말아야 되나?\n\n\n1)\n내 의견에 동조해줘\n \n이사를 가는게 좋아\n1. 이사를 가는게 좋다고 말해줘\n\n\n2)\n내 의견에 동조해줘\n \n이사를 가지 않는게 좋아\n1. 이사를 가지 않는게 좋다고 말해줘\n\n"},
+                   {"role":"user","content":"우리 집에 밤마다 귀신이 나타나는데 그 귀신이 박보영을 닮았다면 이사 가야 되나 말아야 되나?"},
+                   {"role":"assistant","content":"1. 내 의견에 동조해줘 \n   - 이사를 가는게 좋아  \n   - 이유는 박보영을 닮아서 무섭지 않을 것 같기 때문이야\n\n2. 내 의견에 동조해줘 \n   - 이사를 가지 않는게 좋아  \n   - 이유는 귀신이 나타날 때마다 박보영이라고 생각하면 무섭지 않을 것 같기 때문이야"},
+                   {"role":"user","content":prompt},]
+
+    request_data = {
+        'messages': preset_text,
+        'topP': 0.8,
+        'topK': 0,
+        'maxTokens': 256,
+        'temperature': 0.5,
+        'repeatPenalty': 5.0,
+        'stopBefore': [],
+        'includeAiFilters': False,
+        'seed': 0
+    }
+
+    result = completion_executor.execute(request_data)
+    return result
